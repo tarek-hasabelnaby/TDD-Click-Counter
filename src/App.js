caf475d48218a9c14520e6068c14530b52f1c31d
Tarek-123 <t.hasab@icloud.com>
2021-04-24 22:49:53 -0400

    init the click counter and install enzyme

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div data-test="component-app">
+      <ul>
+        Let's remove the [data-test] attributes from DOM properties in
+        production mode
+        <li>npm install --save-dev babel-plugin-react-remove-properties</li>
+        <li>npm run eject -- in order to config react manually</li>
+        <li>Update babel config file</li>
+        <li>Serve your app</li>
+      </ul>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,19 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import Enzyme, { shallow } from "enzyme";
+import EnzymeAdapter from "@wojtekmaj/enzyme-adapter-react-17";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+test("render without error", () => {
+  // generate wrapper with shallow fn.
+  const wrapper = shallow(<App />);
+  const appRootComponent = wrapper.find("[data-test='component-app']");
+  // make an assertions
+  expect(appRootComponent.length).toBe(1);
 });
+
+test("renders increment button", () => {});
+test("renders counter display", () => {});
+test("counter display starts at 0", () => {});
+// we test the counter behaviour
+test("clicking button incrments counter display", () => {});
