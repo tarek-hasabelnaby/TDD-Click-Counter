26cf07837ab12074ca5b2711436b775c54c3c18c
Tarek-123 <t.hasab@icloud.com>
2021-04-28 17:10:35 -0400

    No count below 0 and Remove error when increment button is clicked

--- src/App.js
+++ src/App.js
@@ -4,13 +4,45 @@ import { useState } from "react";
 
 function App() {
   const [counter, setCounter] = useState(0);
+  const [err, setErr] = useState(false);
+  const handleDecrementCounter = () => {
+    if (counter === 0) {
+      setErr(true);
+    } else {
+      setCounter(counter - 1);
+      setErr(false);
+    }
+  };
   return (
     <div data-test="component-app">
       <h1 data-test="display-counter">
         The Counter is currently&nbsp; <span data-test="count">{counter}</span>
       </h1>
-      <button data-test="inc-btn" onClick={() => setCounter(counter + 1)}>
-        Increment Counter
+      <div data-test="err-msg" className={`err ${err ? "" : "hidden"}`}>
+        The counter cannot go below 0
+      </div>
+      <button
+        data-test="inc-btn"
+        onClick={() => {
+          if (err) {
+            setErr(false);
+          }
+          setCounter(counter + 1);
+        }}
+      >
+        Increment counter
+      </button>
+      <button
+        data-test="dec-btn"
+        onClick={() => {
+          if (counter > 0) {
+            setCounter(counter - 1);
+          } else {
+            setErr(true);
+          }
+        }}
+      >
+        Decrement Counter
       </button>
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,4 @@
-import Enzyme, { shallow } from "enzyme";
+import Enzyme, { configure, shallow } from "enzyme";
 import EnzymeAdapter from "@wojtekmaj/enzyme-adapter-react-17";
 import App from "./App";
 
@@ -21,31 +21,98 @@ test("render without error", () => {
   expect(appRootComponent.length).toBe(1);
 });
 
-test("renders increment button", () => {
-  const wrapper = setup();
-  const incrementButton = findByTestAttr(wrapper, "inc-btn");
-  // make an assertions
-  expect(incrementButton.length).toBe(1);
-});
-test("renders counter display", () => {
-  const wrapper = setup();
-  const displayCounter = findByTestAttr(wrapper, "display-counter");
-  expect(displayCounter.length).toBe(1);
-});
-test("counter display starts at 0", () => {
-  const wrapper = setup();
-  const count = findByTestAttr(wrapper, "count").text();
-  expect(count).toBe("0");
-});
-// we test the counter behaviour
-test("clicking button incrments counter display", () => {
-  const wrapper = setup();
-  // find the button
-  const incrementButton = findByTestAttr(wrapper, "inc-btn");
-  // click the button --> using simulate(event ...)
-  incrementButton.simulate("click");
+describe("Increment", () => {
+  test("renders increment button", () => {
+    const wrapper = setup();
+    const incrementButton = findByTestAttr(wrapper, "inc-btn");
+    // make an assertions
+    expect(incrementButton.length).toBe(1);
+  });
+  test("renders counter display", () => {
+    const wrapper = setup();
+    const displayCounter = findByTestAttr(wrapper, "display-counter");
+    expect(displayCounter.length).toBe(1);
+  });
+  test("counter display starts at 0", () => {
+    const wrapper = setup();
+    const count = findByTestAttr(wrapper, "count").text();
+    expect(count).toBe("0");
+  });
+  // we test the counter behaviour
+  test("clicking button incrments counter display", () => {
+    const wrapper = setup();
+    // find the button
+    const incrementButton = findByTestAttr(wrapper, "inc-btn");
+    // click the button --> using simulate(event ...)
+    incrementButton.simulate("click");
 
-  // find the display, and test that number has been incremented
-  const count = findByTestAttr(wrapper, "count").text();
-  expect(count).toBe("1");
+    // find the display, and test that number has been incremented
+    const count = findByTestAttr(wrapper, "count").text();
+    expect(count).toBe("1");
+  });
+});
+
+describe("decrement button", () => {
+  test("render decrement button", () => {
+    const wrapper = setup();
+    const decrementButton = findByTestAttr(wrapper, "dec-btn");
+    expect(decrementButton.length).toBe(1);
+  });
+
+  test("clicking decrement button decrements counter display when state is greater than 0", () => {
+    const wrapper = setup();
+    const incrementButton = findByTestAttr(wrapper, "inc-btn");
+    incrementButton.simulate("click");
+    const decrementCounter = findByTestAttr(wrapper, "dec-btn");
+    decrementCounter.simulate("click");
+    const count = findByTestAttr(wrapper, "count").text();
+    expect(count).toBe("0");
+  });
+});
+
+describe("error when counter goes below 0", () => {
+  test("error does not show when not needed", () => {
+    const wrapper = setup();
+    const errorDiv = findByTestAttr(wrapper, "err-msg");
+    const errorHasHiddenClass = errorDiv.hasClass("hidden");
+    expect(errorHasHiddenClass).toBe(true);
+  });
+});
+
+describe("counter is 0 and decement is clicked", () => {
+  // using describe here so I can use "before ech" for shared setup
+
+  // scoping wrapper to the describe
+  let wrapper;
+  beforeEach(() => {
+    wrapper = setup();
+    // find the buuton
+    const decrementButton = findByTestAttr(wrapper, "dec-btn");
+    decrementButton.simulate("click");
+  });
+
+  test("error shows", () => {
+    // check the class for the error
+    const errorDiv = findByTestAttr(wrapper, "err-msg");
+    // using enzyme's ".hasClass()" method
+    // http://airbnb.io/enzyme/docs/api/ShallowWrapper/hasClass.html
+    const errorHasHiddenClass = errorDiv.hasClass("hidden");
+    expect(errorHasHiddenClass).toBe(false);
+  });
+
+  test("counter still displays 0", () => {
+    const count = findByTestAttr(wrapper, "count").text();
+    expect(count).toBe("0");
+  });
+
+  test("clicking increment clears the error", () => {
+    // find and click the increment button
+    const incButton = findByTestAttr(wrapper, "inc-btn");
+    incButton.simulate("click");
+
+    // check the class of the error message
+    const errorDiv = findByTestAttr(wrapper, "err-msg");
+    const errorHasHiddenClass = errorDiv.hasClass("hidden");
+    expect(errorHasHiddenClass).toBe(true);
+  });
 });
